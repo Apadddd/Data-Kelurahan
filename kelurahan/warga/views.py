5494d50587f2dde4782405b7a6ba6ec0d369eb66
Apadddd <marmutm114@gmail.com>
2025-11-13 11:44:11 +0700

    Pertemuan 7

--- kelurahan/warga/views.py
+++ kelurahan/warga/views.py
@@ -6,6 +6,12 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Warga, Pengaduan
 from .forms import WargaForm
+# from rest_framework.generics import ListAPIView, RetrieveAPIView
+# from .serializers import WargaSerializer, PengaduanSerializer
+from rest_framework import viewsets # Impor viewsets
+from .serializers import WargaSerializer, PengaduanSerializer
+from .models import Warga, Pengaduan
+
 
 class WargaListView(ListView):
     model = Warga
@@ -49,3 +55,30 @@ class PengaduanDeleteView(DeleteView):
     model = Pengaduan
     template_name = 'warga/pengaduan_confirm_delete.html'
     success_url = reverse_lazy('pengaduan-list')
+
+# class WargaListAPIView(ListAPIView):
+#    queryset = Warga.objects.all()
+#    serializer_class = WargaSerializer
+
+# class WargaDetailAPIView(RetrieveAPIView):
+#    queryset = Warga.objects.all()
+#    serializer_class = WargaSerializer
+
+# class PengaduanListAPIView(ListAPIView):
+#     queryset = Pengaduan.objects.all()
+#     serializer_class = PengaduanSerializer
+
+# --- API VIEWS ---
+class WargaViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Warga.objects.all().order_by('-tanggal_registrasi')
+    serializer_class = WargaSerializer
+
+class PengaduanViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Pengaduan.objects.all().order_by('-id')
+    serializer_class = PengaduanSerializer
